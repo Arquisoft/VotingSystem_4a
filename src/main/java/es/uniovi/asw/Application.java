9e7bb5160e23567de188a96daf1f1dd42ccb0c63
Iván <ivan_ma@telecable.es>
2016-03-31 06:04:55 +0200

    Cambios menores

--- src/main/java/es/uniovi/asw/Application.java
+++ src/main/java/es/uniovi/asw/Application.java
@@ -9,9 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-  public static void main(String[] args) {
-    SpringApplication app = new SpringApplication(Application.class);
-    app.setBannerMode(Banner.Mode.OFF);
-    app.run(args);
-  }
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(Application.class);
+		app.setBannerMode(Banner.Mode.OFF);
+		app.run(args);
+	}
+
 }
