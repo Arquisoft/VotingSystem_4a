103afe1f15af6b22938d8834a0151a3e71542b50
Iván <ivan_ma@telecable.es>
2016-04-03 00:03:01 +0200

    Versión inicial

---
+++ src/main/java/es/uniovi/asw/dbupdate/repositories/CandidatureRepository.java
@@ -0,0 +1,12 @@
+package es.uniovi.asw.dbupdate.repositories;
+
+import es.uniovi.asw.model.Candidature;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * CandidatureRepository
+ * Created by ivan on 2/04/16.
+ */
+public interface CandidatureRepository extends CrudRepository<Candidature, Long> {
+
+}
