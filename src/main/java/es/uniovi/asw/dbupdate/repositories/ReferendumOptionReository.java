03d30304a1b14fe722fb4a24bf064fb6cc348ebc
Iván <ivan_ma@telecable.es>
2016-04-03 00:04:27 +0200

    Corregido el nombre del repositorio

--- src/main/java/es/uniovi/asw/dbupdate/repositories/ReferendumOptionReository.java
+++
@@ -1,9 +0,0 @@
-package es.uniovi.asw.dbupdate.repositories;
-
-/**
- * ReferendumOptionReository
- * Created by ivan on 2/04/16.
- */
-public interface ReferendumOptionReository extends CandidatureRepository {
-
-}
