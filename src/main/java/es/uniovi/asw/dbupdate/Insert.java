5ee9200d7f4e3b2f1a646f89cd54bb316de2da73
Iván <ivan_ma@telecable.es>
2016-04-08 06:38:30 +0200

    Implementada la inserción en la BB.DD: verifica que los datos recibidos son correctos y establece las relaciones entre las entidades

--- src/main/java/es/uniovi/asw/dbupdate/Insert.java
+++ src/main/java/es/uniovi/asw/dbupdate/Insert.java
@@ -1,9 +1,19 @@
 package es.uniovi.asw.dbupdate;
 
+import es.uniovi.asw.model.Election;
+import es.uniovi.asw.model.ElectionCall;
+import es.uniovi.asw.util.ParametersException;
+
 /**
  * Insert
  * Created by ivan on 1/04/16.
  */
 public interface Insert {
+
+	void insertElectionCall(ElectionCall electionCall) throws ParametersException;
+
+	void insertElection(Long idElectionCall, Election election) throws ParametersException;
+
+
 
 }
--- src/main/java/es/uniovi/asw/dbupdate/ports/InsertP.java
+++ src/main/java/es/uniovi/asw/dbupdate/ports/InsertP.java
@@ -1,6 +1,14 @@
 package es.uniovi.asw.dbupdate.ports;
 
 import es.uniovi.asw.dbupdate.Insert;
+import es.uniovi.asw.dbupdate.ports.verifiers.ElectionCallVerifier;
+import es.uniovi.asw.dbupdate.ports.verifiers.ElectionVerifier;
+import es.uniovi.asw.dbupdate.repositories.ElectionCallRepository;
+import es.uniovi.asw.dbupdate.repositories.ElectionRepository;
+import es.uniovi.asw.model.Election;
+import es.uniovi.asw.model.ElectionCall;
+import es.uniovi.asw.util.ParametersException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -9,5 +17,28 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class InsertP implements Insert {
+
+	@Autowired
+	private ElectionCallRepository electionCallRepository;
+
+	@Autowired
+	private ElectionRepository electionRepository;
+
+	@Override
+	public void insertElectionCall(ElectionCall electionCall) throws ParametersException {
+		ElectionCallVerifier.verify(electionCall, electionCallRepository);
+		electionCallRepository.save(electionCall);
+	}
+
+	@Override
+	public void insertElection(Long idElectionCall, Election election) throws ParametersException {
+		ElectionVerifier.verify(election, electionRepository);
+
+		ElectionCall electionCall = electionCallRepository.findOne(idElectionCall);
+		ElectionCallVerifier.verify(electionCall, electionCallRepository);
+
+		election.setElectionCall(electionCall);
+		electionRepository.save(election);
+	}
 
 }
