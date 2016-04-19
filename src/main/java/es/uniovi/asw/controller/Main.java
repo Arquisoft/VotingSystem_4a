8e384c1fd81c941116eb747e3e69566d53afe9c8
risuarez <rsrodriguez19@hotmail.com>
2016-04-19 12:54:28 +0200

    travis

--- src/main/java/es/uniovi/asw/controller/Main.java
+++ src/main/java/es/uniovi/asw/controller/Main.java
@@ -5,16 +5,20 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import es.uniovi.asw.model.Voter;
 
 @Controller
 public class Main {
 
 	private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
-	@RequestMapping("/")
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public ModelAndView landing(Model model) {
 		logger.info("Index page access");
+		model.addAttribute("voter", new Voter());
 		return new ModelAndView("index");
 	}
 
