bf935b58bd98f808a6a9710b990ea7525ad18c0a
Iván <ivan_ma@telecable.es>
2016-04-20 01:31:04 +0200

    Añadidos los repositorios de las candidaturas

---
+++ src/main/java/es/uniovi/asw/dbupdate/repositories/ReferendumOptionRepository.java
@@ -0,0 +1,11 @@
+package es.uniovi.asw.dbupdate.repositories;
+
+import es.uniovi.asw.model.Candidature;
+
+/**
+ * ReferendumOptionRepository
+ * Created by ivan on 2/04/16.
+ */
+public interface ReferendumOptionRepository extends CandidatureRepository {
+
+}
