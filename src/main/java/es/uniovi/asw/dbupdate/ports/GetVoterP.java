653fa340186c7ed342a026ab86e2952d935bae94
Iván <ivan_ma@telecable.es>
2016-04-20 01:58:10 +0200

    Añadida la implementación de GetVoter y las comprobaciones de id que faltaban

--- src/main/java/es/uniovi/asw/dbupdate/ports/GetVoterP.java
+++ src/main/java/es/uniovi/asw/dbupdate/ports/GetVoterP.java
@@ -1,5 +1,11 @@
 package es.uniovi.asw.dbupdate.ports;
 
+import es.uniovi.asw.dbupdate.GetVoter;
+import es.uniovi.asw.dbupdate.ports.verifiers.VoterVerifier;
+import es.uniovi.asw.dbupdate.repositories.VoterRepository;
+import es.uniovi.asw.model.Voter;
+import es.uniovi.asw.util.ParametersException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -7,6 +13,22 @@ import org.springframework.stereotype.Service;
  * Created by ivan on 2/04/16.
  */
 @Service
-public class GetVoterP {
+public class GetVoterP implements GetVoter {
+
+	@Autowired
+	private VoterRepository voterRepository;
+
+	@Override
+	public Voter getVoter(Long idVoter) throws ParametersException {
+
+		if (idVoter == null) {
+			throw new ParametersException("El id de votante es nulo");
+		}
+
+		Voter voter = voterRepository.findOne(idVoter);
+		VoterVerifier.verify(voter);
+
+		return voter;
+	}
 
 }
