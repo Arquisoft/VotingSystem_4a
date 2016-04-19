86a5ed10d4f3894ff516fcf201f9a0e0f3105e12
Iván <ivan_ma@telecable.es>
2016-04-20 01:58:25 +0200

    Añadida la definición de GetVoter

--- src/main/java/es/uniovi/asw/dbupdate/GetVoter.java
+++ src/main/java/es/uniovi/asw/dbupdate/GetVoter.java
@@ -1,9 +1,13 @@
 package es.uniovi.asw.dbupdate;
 
+import es.uniovi.asw.model.Voter;
+import es.uniovi.asw.util.ParametersException;
+
 /**
  * GetVoter
  * Created by ivan on 2/04/16.
  */
 public interface GetVoter {
 
+	Voter getVoter(Long idVoter) throws ParametersException;
 }
